6f95e287f2f82826af50aec78834e3bc35eca8b3
ashsometimes <aishsahoo5@gmail.com>
2020-09-17 09:42:47 +0530

    done

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -106,9 +106,6 @@ class Contact extends Component {
 
                   <div>
                      <button type="submit" className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
                   </div>
 					</fieldset>
 				   </form>
